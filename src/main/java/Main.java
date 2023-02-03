3e9a842596f02ef68112041145485aa320f791dd
Be1t0k <pasha.moshckov@yandex.ru>
2023-02-03 14:13:59 +0300

    set headless but input isn't clickable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
@@ -18,15 +19,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.edge.driver", "./src/main/resources/msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
-        WebDriver webDriver = new EdgeDriver();
+        EdgeOptions options = new EdgeOptions();
+        options.setHeadless(true);
+        options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        WebDriver driver = new EdgeDriver(options);
+        WebDriver webDriver = new EdgeDriver(options);
         Parser parser = new Parser();
-        //parser.GOG(driver);
-        //parser.Steam(webDriver);
         Scanner inn = new Scanner(System.in);
-        ExecutorService executor = Executors.newCachedThreadPool();
-        executor.execute(() -> parser.GOG(driver, inn));
-        executor.execute(() -> parser.Steam(webDriver, inn)); //inline
-        executor.shutdown();
+        //parser.GOG(driver,inn);
+        parser.Steam(webDriver,inn);
+//        ExecutorService executor = Executors.newCachedThreadPool();
+//        executor.execute(() -> parser.GOG(driver, inn));
+//        executor.execute(() -> parser.Steam(webDriver, inn)); //inline
+//        executor.shutdown();
     }
 }
